cb10aa7241667513180ad273ee67737d33edf077
Isaac Mendez <isaac.mendez@siproset.com>
2025-06-14 19:53:01 -0600

    mujoco test

---
+++ src/mujoco-test.py
@@ -0,0 +1,11 @@
+import mujoco
+model = mujoco.MjModel.from_xml_string("<mujoco><worldbody></worldbody></mujoco>")
+data = mujoco.MjData(model)
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    step = 0
+    while viewer.is_running():
+        step += 1
+        data.ctrl[:] = 0.0  # No control input
+        mujoco.mj_step(model, data)
+        viewer.sync()
+        viewer.render()
